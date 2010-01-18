dcc6a93b933802bbfb5ac8e60fb9ad39a9b8f4ff
bphiggin01 <>
2010-01-18 13:03:25 -0600

    g

--- src/com/uhs/test/TestClass.java
+++ src/com/uhs/test/TestClass.java
@@ -2,4 +2,13 @@ package com.uhs.test;
 
 public class TestClass {
 	private String test;
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
 }
